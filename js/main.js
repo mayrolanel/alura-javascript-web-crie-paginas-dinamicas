0530d7a8b6e35a8fee1ced6a42ffec57dfeb1d17
Mayro Lanel da Silva Belizario <mayro.belizario@GTC-N-795.trinuscapital.com>
2023-08-04 18:45:01 -0300

    refactor: aula-01 conhecendo o javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let tecla_pom = document.querySelector('.tecla_pom')
+let tecla_clap = document.querySelector('.tecla_clap')
+let tecla_tim = document.querySelector('.tecla_tim')
+let tecla_puff = document.querySelector('.tecla_puff')
+let tecla_splash = document.querySelector('.tecla_splash')
+let tecla_toim = document.querySelector('.tecla_toim')
+let tecla_psh = document.querySelector('.tecla_psh')
+let tecla_tic = document.querySelector('.tecla_tic')
+let tecla_tom = document.querySelector('.tecla_tom')
+
